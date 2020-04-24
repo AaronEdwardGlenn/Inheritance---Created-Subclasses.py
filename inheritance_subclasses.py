012538ea6a17733a7708b9627e96290f80d83eb8
Aaron Edward Glenn <53454869+AaronEdwardGlenn@users.noreply.github.com>
2020-04-24 14:13:18 -0700

    added new subclasses that inherit from parent class and add unique methods and attributes.

--- inheritance_subclasses.py
+++ inheritance_subclasses.py
@@ -18,13 +18,43 @@ class Employee:
     def apply_raise(self):
         self.pay = int(self.pay * self.raise_amount)
 
+# we can give the developer class its own init method to instantiate all instances of it will defaults.
+
 
 class Developer(Employee):
+    def __init__(self, first, last, pay, prog_lang):
+        # NOTE super is required here to tell the code to find these other class attributes from the parent.
+        super().__init__(first, last, pay)
+        self.prog_lang = prog_lang
     raise_amount = 1.8
 
 
-dev_1 = Developer('Aaron', 'Glenn', 100)
-dev_2 = Employee('Test', 'User', 200)
+class Manager(Employee):
+    def __init__(self, first, last, pay, employees=None):
+        super().__init__(first, last, pay)
+        if employees is None:
+            self.employees = []
+        else:
+            self.employees = employees
+    raise_amount = 5
+
+    def add_employee(self, employee):
+        if employee not in self.employees:
+            self.employees.append(employee)
+
+    def remove_employee(self, employee):
+        if employee in self.employees:
+            self.employees.remove(employee)
+
+    def print_employees(self):
+        for employees in self.employees:
+            print('-->', employee.fullname())
+
+
+dev_1 = Developer('Aaron', 'Glenn', 100, 'Python')
+dev_2 = Developer('Test', 'User', 200, 'Java')
+
+mananger_1 = Manager('Calvin', 'Coolidge', 500, [dev_1])
 
 print(dev_1.email)
 print(dev_2.email)
@@ -34,3 +64,11 @@ print(dev_2.email)
 print(dev_1.pay)
 dev_1.apply_raise()
 print(dev_1.pay)
+
+print(dev_1.prog_lang)
+
+print(mananger_1.pay)
+mananger_1.apply_raise()
+print(mananger_1.pay)
+
+print(mananger_1.email)
